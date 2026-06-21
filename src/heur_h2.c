8ae0a71ee377793cf0618ea0d9ef3c0b4c234138
wefjyl <af44kt@student.aau.dk>
2026-06-21 10:25:22 +0200

    fixed wording in print of search node

--- src/heur_h2.c
+++ src/heur_h2.c
@@ -21,7 +21,7 @@ static int heurEstimate(pddl_heur_t *_h,
                         const pddl_fdr_state_space_t *state_space)
 {
     pddl_heur_h2_t *h = pddl_container_of(_h, pddl_heur_h2_t, heur);
-    printf("\nCalling heuristic function for search node of state ");
+    printf("\nCalling heuristic function for search node with state ");
     for (int i = 0; i < node->var_size; i++) {
         int fact_id = node->state[i];
         printf("%d, ", fact_id);
